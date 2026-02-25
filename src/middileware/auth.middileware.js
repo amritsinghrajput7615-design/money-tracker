be9b34dea606723ac75aae4a3ecdaaf7fc38cf62
Amrit Kumar <amritsinghrajput7615@gmail.com>
2026-02-25 21:26:38 +0530

    fix auth middleware

--- src/middileware/auth.middileware.js
+++ src/middileware/auth.middileware.js
@@ -1,21 +1,33 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
 
-function authMiddileware(req,res,next){
-    const token = req.cookies?.token || req.headers.authorization?.split(" ")[1]
-    if(!token){
-        return res.status(400).json({
-            message : "no token"
-        })
+async function authMiddileware(req, res, next) {
+  const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "No token provided",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // 🔥 VERY IMPORTANT
+    const user = await User.findById(decoded.id)
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
     }
-    try {
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
-    req.user= decoded
-    next()
-    } catch (error) {
-        return res.status(400).json({
-            message : "invalid token"
-        })
-    }
-    
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid token",
+    });
+  }
 }
-module.exports = authMiddileware
+
+module.exports = authMiddileware;
